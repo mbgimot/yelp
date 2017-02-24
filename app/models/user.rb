183599ff6fc6d8bf1fdca6fd0d3288e391a1944b
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-02-24 19:58:37 +0000

    removed commented code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   has_many :restaurants
   has_many :reviews, dependent: :destroy
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
@@ -11,11 +10,6 @@ class User < ApplicationRecord
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.email = auth.info.email
        user.password = Devise.friendly_token[0,20]
-      #  user.name = auth.info.name   # assuming the user model has a name
-      #  user.image = auth.info.image # assuming the user model has an image
-       # If you are using confirmable and the provider(s) you use validate emails,
-       # uncomment the line below to skip the confirmation emails.
-       # user.skip_confirmation!
      end
    end
 
